5bdcfb48a2e05dbd13cc24ad5081aa1a94346e2c
AkramGafurov <akramminsk@yander.ru>
2020-10-11 22:09:38 +0300

    feat: finish revers str

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    n = String(Math.abs(n)).split('');
+    let indStart = 0;
+    let indexEnd = n.length - 1;
+    while (indStart < indexEnd) {
+        [n[indStart], n[indexEnd]] = [n[indexEnd], n[indStart]];
+        indStart++;
+        indexEnd--
+    }
+    return n.join('')
 }
+
+// console.log(reverse());
